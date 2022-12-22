5c402f0f9a7ba50e3d8167a35d08030a6889c83d
Cristian Yanza Velasco <cristian.yanzav@perficient.com>
2022-12-22 14:32:23 -0500

    initial commit

---
+++ src/main/java/com/fundamentals/springboot/fundamentals/FundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentals.springboot.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentals/springboot/fundamentals/FundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentals.springboot.fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
